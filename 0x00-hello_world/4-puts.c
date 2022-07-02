543c5457ebad9c21ec15d83965788d2cd7c608e0
Nuwabiine Bonaventure <mnuwabiine@gmail.com>
2022-07-02 06:03:25 -0700

    Adding files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include "stdio.h"
-
 int main(void)
 {
-  puts("Programming is like building a mutilingual puzzle \n");
-  return(0);
+puts ("Programming is like building a mutilingual puzzle \n");
+return(0);
 }
