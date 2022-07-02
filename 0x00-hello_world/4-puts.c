983243a3ceea728a9ba2a4cfed5dcaeb2c636925
Nuwabiine Bonaventure <mnuwabiine@gmail.com>
2022-07-02 07:03:26 -0700

    Adding corrected files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
-#include "stdio.h"
+#include <stdio.h>
+
 int main(void)
 {
-puts("Programming is like building a mutilingual puzzle \n");
-return(0);
+puts("\"Programming is like building a mutilingual puzzle \n");
+return (0);
 }
