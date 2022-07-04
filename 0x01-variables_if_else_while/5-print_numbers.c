0c4130b8e0082c749d1b7772956e63a93b0188ae
Nuwabiine Bonaventure <mnuwabiine@gmail.com>
2022-07-04 05:50:12 -0700

    Files to be commited

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,11 +9,12 @@
 
 int main(void)
 {
-	int i = 0;
+	int n = 0;
 
-	for (i = 0; i < 10; i++)
+	for (n = 0; n < 10; n++)
 	{
-		printf("%d\n", i);
+		printf("%d", n);
 	}
+	printf("\n");
 	return (0);
 }
