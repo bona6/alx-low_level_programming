736f1779c79ffb9d7d455a447dca09f37a3b208d
Nuwa Bona <mnuwabiine@gmail.com>
2022-07-29 13:42:42 +0300

    More malloc free assignment is done

---
+++ 0x0C-more_malloc_free/_memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte
+ * @new_ptr: the pointer
+ * @c: the char
+ * @size: the amount of memory being filled
+ * Return: the new memory
+ */
+char *_memset(char *new_ptr, char c, unsigned int size)
+{
+char *ptr = new_ptr;
+
+while (size--)
+{
+*ptr++ = c;
+}
+return (new_ptr);
+}
