4aa9ddadbbc4956c3f569d98f2e7c381d7f0fa09
Nuwabiine Mbiine Bona <nmbiine@gmail.com>
2023-02-17 03:40:36 -0800

    correcting print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,29 +11,22 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 48; i < 58; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 48; j < 58; j++)
+		for (j = 0; j < 10; j++)
 		{
-			if (i == j)
+			if (j > i)
 			{
-				continue;
-			}
-			putchar(i);
-			putchar(j);
-			if (i == 56 && j == 57)
-			{
-				break;
-			}
-			else
-			{
-				putchar(44);
-				putchar(32);
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i != 8 && j !== 9)
+				{
+				putchar(',');
+				putchar(' ');
+				}
 			}
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
-
