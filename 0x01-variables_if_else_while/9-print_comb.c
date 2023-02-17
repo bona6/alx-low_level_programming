5071e6a798b86664e44774b6bfb6cb5a8dc53bdc
Nuwabiine Mbiine Bona <nmbiine@gmail.com>
2023-02-17 03:27:16 -0800

    Correcting print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,17 +9,16 @@
 
 int main(void)
 {
-	int i;
-
-	for (i = 48; i <= 57; i++)
-	{
-		putchar(i);
-        	if (i != 57)
-        	{
-	        	putchar(44);
-	        	putchar(32);
-        	}
-        }
-	putchar(10);
-	return (0);
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+if (i != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+putchar(10);
+return (0);
 }
