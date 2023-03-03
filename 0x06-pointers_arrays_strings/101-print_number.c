675d4f8507092de6b56ebe16561272f95c0b7c26
Nuwabiine Mbiine Bonaventure <92671469+Bona96@users.noreply.github.com>
2023-03-03 22:46:19 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,14 +7,19 @@
  */
 void print_number(int n)
 {
-	unsigned int x = n;
+	unsigned int n1;
 
 	if (n < 0)
 	{
+		n1 = -n;
 		_putchar('-');
-		x = -x;
+	} else
+	{
+		n1 = n;
 	}
-	if ((x / 10) > 0)
-		print_number(x / 10);
-	_putchar((x % 10) + '0');
+
+	if (n1 / 10)
+		print_number(n1 / 10);
+
+	_putchar((n1 % 10) + '0');
 }
