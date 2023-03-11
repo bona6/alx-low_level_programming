847d07f0c36e74fdcec3949de926b28732278ccf
Nuwabiine Mbiine Bonaventure <92671469+Bona96@users.noreply.github.com>
2023-03-11 15:47:48 +0300

    print the largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,24 +10,20 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+int largest;
 
-	if (a > b && a > c)
-	{
-		largest = a;
-	}
-	else if (a > b && c > a)
-	{
-		largest = c;
-	}
-	else if (b > c)
-	{
+if (a > b && a > c)
+	largest = a;
+else if (a > c && c > b)
+	largest = a;
+else if (b > a && a > c)
 	largest = b;
-	}
-	else
-	{
+else if (b > c && c > a)
+	largest = b;
+else if (c > a && a > b)
 	largest = c;
-	}
+else
+	largest = c;
 
-	return (largest);
+return (largest);
 }
