2103ef28a319132ed9b2699cbd5e1d3bfd328bac
bona6 <127660659+bona6@users.noreply.github.com>
2023-03-12 16:49:19 +0300

    print a number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,23 +1,20 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n:.input integer parameter
- *
- *
+ *print_number - prints integers
+ *@n: number to be printed
+ *Return: nothing
  */
 void print_number(int n)
 {
-	unsigned int i = n;
+	unsigned int x = n;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		i = -i;
+		_putchar('-');
+		x = -x;
 	}
-	if (i / 10)
-	{
-		print_number(i / 10);
-	}
-	_putchar(i % 10 + '0');
+	if ((x / 10) > 0)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
